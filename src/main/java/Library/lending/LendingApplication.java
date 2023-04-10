580415d8b4cbb1876fef850ef0c77878cf29df9c
ssxkalce <halina1900@yahoo.com>
2023-04-10 19:12:22 +0200

    Initial commit

---
+++ src/main/java/Library/lending/LendingApplication.java
@@ -0,0 +1,13 @@
+package Library.lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Library/lending/LendingApplicationTests.java
@@ -0,0 +1,13 @@
+package Library.lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
